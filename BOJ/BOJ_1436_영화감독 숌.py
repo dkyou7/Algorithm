fcdd9f308945a6f87b676f4d4f230b103ccf31b5
youdongkwan <dkyou7@naver.com>
2022-01-02 22:53:40 +0900

    [python] BOJ_1436_영화감독 숌.py

---
+++ BOJ/BOJ_1436_영화감독 숌.py
@@ -0,0 +1,11 @@
+name = 665
+counter = 0
+result = int(input())
+
+while True:
+    if "666" in str(name):
+        counter += 1
+        if result == counter:
+            print(name)
+            break
+    name += 1
